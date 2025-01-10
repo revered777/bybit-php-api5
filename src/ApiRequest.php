4935aaf7ea12d53e43d2fd7b229acea2f966d131
revered777 <blogahh@gmail.com>
2025-01-10 18:31:13 +0300

    fix

--- src/ApiRequest.php
+++ src/ApiRequest.php
@@ -16,7 +16,7 @@ class ApiRequest {
     /**
      * @var string ByBit API RECV Window
      */
-    const BAPI_RECV_WINDOW = "5000";
+    const BAPI_RECV_WINDOW = "20000";
     
     /**
      * @var string ByBit API Sign Type
